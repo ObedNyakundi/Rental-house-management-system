7ff0070b3755d893bcae3e97d61847a0efd7857d
Obed Nyakundi <real.obedpaul@gmail.com>
2025-01-25 12:46:19 +0300

    introduced a login button on the landing

--- index.php
+++ index.php
@@ -55,6 +55,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							<li><a href="portfolio.php">Products</a></li>
 							<li><a href="blog.php">Blog</a></li>
 							<li><a href="contact.php">Contact</a></li>
+							<li><a href="./admin">Login</a></li>
 						</ul>
 						
 					</nav>
